5831a503d231c5437894ef7548705aa530be0ef1
0zelot <29740364+0zelot@users.noreply.github.com>
2023-01-02 20:40:06 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export default {
+    widget_url: "https://discord.com/api/guilds/GUILD_ID/widget.json",
+    webhook_url: "https://discord.com/api/webhooks/CHANNEL_ID/WEBHOOK_TOKEN",
+    role_id: "ROLE_ID",
+    bot: {
+        username: "BOT_USERNAME",
+        avatar_url: "BOT_AVATAR_URL"
+    },
+    commands: []
+}

8270bff38d8606bdb3688aab2e96115d54e62a37
Ozelot <29740364+0zelot@users.noreply.github.com>
2023-01-02 20:28:26 +0100

    Creating data file if not exists

--- utils/status.js
+++ utils/status.js
@@ -1,14 +1,18 @@
 import fs from "node:fs/promises";
 
-const getStatus = async () => JSON.parse(await fs.readFile("./data.json"));
+const getStatus = async () => {
+    try {
+        return JSON.parse(await fs.readFile("./data.json"));
+    } catch {
+        await fs.writeFile("./data.json", JSON.stringify({}));
+    }
+}
 
 const setStatus = async (status) => {
-
     await fs.writeFile("./data.json", JSON.stringify({
         status,
         since: Date.now()
     }, null, 2));
-
 }
 
 export {getStatus, setStatus}
